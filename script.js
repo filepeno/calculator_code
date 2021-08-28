bc2dd08d5bdea0e8d3c4131510100d60e3cb6011
Filip <fili4252@stud.kea.dk>
2021-08-28 15:34:39 +0200

    reault appears in first input field and added comments

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ const calculateBtn = document.querySelector("#calculate");
 
 setup();
 
+//add eventlistener for calculate button
 function setup() {
   console.log("setup()");
   calculateBtn.addEventListener("click", readInput);
 }
 
+//read both input fields
 function readInput(e) {
   console.log("readInput()");
   e.preventDefault();
@@ -29,6 +31,7 @@ function readInput(e) {
 //   console.log("decideCalculation()");
 // }
 
+//calculates first and second number together
 function calculate() {
   console.log("calculate()");
   result = Number(firstnumber.value) + Number(secondnumber.value);
@@ -36,7 +39,16 @@ function calculate() {
   printResult();
 }
 
+//print result at the end of the list
 function printResult() {
   console.log("printResult()");
   list.lastElementChild.textContent = result;
+  updateFirstnumber();
+}
+
+//updates value in the first input field with the result
+function updateFirstnumber() {
+  console.log("updateFirstnumber()");
+  firstnumber.value = result;
+  console.log(firstnumber.value);
 }
